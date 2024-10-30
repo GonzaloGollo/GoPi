dcfa6c828b392b2936d559f0001c5d1a0c46ff6a
GonzaloGollo <gonzalogollo@gmail.com>
2024-10-30 10:57:29 -0300

    Basic Users Api. Comienzo Controller

---
+++ internal/user/controller.go
@@ -0,0 +1,10 @@
+package user
+
+//transformamos a json
+
+type User struct {
+	ID        uint64 `json:"id"`
+	FirstName string `json:"first_name"`
+	LastName  string `json:"last_name"`
+	Email     string `json:"email"`
+}
